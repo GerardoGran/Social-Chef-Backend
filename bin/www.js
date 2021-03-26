32dea310b13a75b8a3cae00e070b963190f31d45
Gerardo Granados Aldaz <gerardogr.al@outlook.com>
2021-03-25 20:40:33 -0700

    gitignore update

--- bin/www.js
+++ bin/www.js
@@ -1,6 +1,6 @@
 const { app } = require('../app');
 
 // set port, listen for requests
-app.listen(3000, () => {
-  console.log("Server is running on port 3000.");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running...");
 });
