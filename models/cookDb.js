d6b8d7e2b7e9fdbbd6b0aa4f4e89cfd354f79004
Gerardo Granados Aldaz <gerardogr.al@outlook.com>
2021-03-25 23:32:44 -0700

    Fix SQL Connection close

--- models/cookDb.js
+++ models/cookDb.js
@@ -1,14 +1,21 @@
+const { connect } = require('../routes');
 const sql = require('./db')
 
 const insertCook = async (fname, lname, username, email, password) => {
     console.log("DB")
     const newCook = { fname, lname, username, email, password }
-    sql.query("INSERT INTO COOKS SET ?", newCook, (err, res) => {
-        if (err) {
-            console.log("error: ", err);
-            return;
-        }
-        console.log("created cook: ", { id: res.insertId, ...newCook });
+
+    sql.getConnection((err, connection) => {
+        if (err) console.error(err);
+        console.log('MySQL Connection establiched: ', connection.threadId);
+        connection.query("INSERT INTO COOKS SET ?", newCook, (err, results) => {
+            if (err) {
+                console.log("error: ", err);
+                return;
+            }
+            console.log("created cook: ", { id: results.insertId, ...newCook });
+            connection.release(err => { if (err) console.error(err) });
+        });
     });
 }
 
--- models/db.js
+++ models/db.js
@@ -1,19 +1,12 @@
 const sql = require("mysql")
 const dbConfig = require("../config/db.config");
-const { connect } = require("../routes");
 
 // Create connection to the db
-var connection = sql.createConnection({
+var connection = sql.createPool({
 	host: dbConfig.HOST,
 	user: dbConfig.USER,
 	password: dbConfig.PASSWORD,
 	database: dbConfig.DB
 });
-
-// Open MySQL connection
-connection.connect(error => {
-	if (error) throw error;
-	console.log("Succesfully connected to the database")
-})
 
 module.exports = connection;
